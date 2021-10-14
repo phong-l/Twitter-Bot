e0db7ed38946fe630e81b548346276ddcfbd6f3e
phong-l <92543616+phong-l@users.noreply.github.com>
2021-10-14 19:58:08 +0100

    Delete src directory

--- src/Image.py
+++
@@ -1,7 +0,0 @@
-from svglib.svglib import svg2rlg
-from reportlab.graphics import renderPM
-
-
-def svgtopng(pic):
-    drawing = svg2rlg(pic)
-    renderPM.drawToFile(drawing, 'temp.png', fmt='png')
